791a2a3cd057deccd78f02302b7b81bdbe3dc722
vohraaYatinn <vohrayatin9@gmail.com>
2024-05-13 12:30:20 +0530

    ss

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-        "http://127.0.0.1:5175"
+        "http://127.0.0.1:5173"
 ]
 CORS_ALLOW_METHODS = [
     'DELETE',
@@ -148,10 +148,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:5175",]
-CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:5175',]
+CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:5173",]
+CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:5173',]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/home/ubuntu/djangoa-app/media/'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from backend import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 
@@ -14,3 +16,6 @@ urlpatterns = [
     path('api/v2/subscriptions/', include('subscriptions.urls')),
     path('api/v2/music/', include('music.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
